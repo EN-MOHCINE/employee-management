4fa1e1c1e19f42ce0ac939ca0c0d77f5f667c210
ENNEFAI <98054051+EN-MOHCINE@users.noreply.github.com>
2024-09-01 15:06:30 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Mail;
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// /*
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider and all of them will
+// | be assigned to the "web" middleware group. Make something great!
+// |
+// */
 
 
 Route::fallback(function () {
